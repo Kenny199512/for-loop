0a5b27392b31dc564b74886ac425e871620a5c14
McGravey <3005kenny@gmail.com>
2022-12-16 22:10:07 -0600

    first commit

---
+++ ifelseexamplebetter.cs
@@ -0,0 +1,34 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter shoe size");
+            int ShoeSize = int.Parse(Console.ReadLine());
+            if (ShoeSize == 1)
+            {//then
+
+                Console.WriteLine("Tinyfoot Free");
+
+            }
+            else if (ShoeSize < 13)
+            {
+
+                Console.WriteLine("Little Markdown");
+            }
+            else if (ShoeSize > 13)
+            {
+                Console.WriteLine("Bigfoot Markup");
+            
+              }else{
+                Console.WriteLine("Regularfoot Normal Price");
+        }      //end if
+               
+               
+               
+
+
+        }
+    }
+}
